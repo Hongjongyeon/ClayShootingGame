a1e8d679ec5ed0c21f65ae1c6d77244e67762ece
HongJongYeon <yjgno2@naver.com>
2023-05-17 10:16:21 +0900

    1

--- app/src/main/java/com/example/clayshootinggame/MainActivity.java
+++ app/src/main/java/com/example/clayshootinggame/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.clayshootinggame;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.animation.Animator;
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
@@ -14,20 +17,6 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    public void onClick(View view) {
-
-        public void gameStop() {
-
-        }
-        public void gameStart() {
-
-        }
-        public void shootingStart() {
-        }
-
-
-    }
 
     ImageView iv_gun;
     ImageView iv_bullet;
@@ -42,8 +31,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     double gun_center_x;
     final int NO_OF_CLAYS = 5;
 
-
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ActionBar actionBar = getSupportActionBar();
@@ -52,12 +41,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button btnStop = (Button) findViewById(R.id.btnStop);
         btnStart.setOnClickListener(this);
         btnStop.setOnClickListener(this);
+
         ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.layout);
         screen_height = Resources.getSystem().getDisplayMetrics().heightPixels;
         screen_width = Resources.getSystem().getDisplayMetrics().widthPixels;
+
         iv_gun = new ImageView(this);
         iv_bullet = new ImageView(this);
         iv_clay = new ImageView(this);
+
         iv_gun.setImageResource(R.drawable.gun);
         iv_gun.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
         gun_height = iv_gun.getMeasuredHeight();
@@ -70,6 +62,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bullet_width = iv_bullet.getMeasuredWidth();
         iv_bullet.setVisibility(View.INVISIBLE);
         layout.addView(iv_bullet);
+
         iv_clay.setImageResource(R.drawable.clay);
         iv_clay.setScaleX(0.8f);
         iv_clay.setScaleY(0.8f);
@@ -77,51 +70,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         clay_height = iv_clay.getMeasuredHeight();
         clay_width = iv_clay.getMeasuredWidth();
         layout.addView(iv_clay);
+
         gun_center_x = screen_width * 0.7;
         gun_x = gun_center_x - gun_width * 0.5;
         gun_y = screen_height - gun_height;
+
         iv_gun.setX((float) gun_x);
         iv_gun.setY((float) gun_y);
+
         iv_gun.setClickable(true);
         iv_gun.setOnClickListener(this);
+    }
 
-        public void onClick (View view;){
+        public void onClick (View view){
             if (view.getId() == R.id.btnStart)
                 gameStart();
             else if (view.getId() == R.id.btnStop)
                 gameStop();
             else if (view == iv_gun)
                 shootingStart();
-
         }
 
-    }
-
-    public void onClick(View view) {
-        if (view.getId() == R.id.btnStart)
-            gameStart();
-        else if (view.getId() == R.id.btnStop)
-            gameStop();
-        else if (view == iv_gun)
-            shootingStart();
-    }
-
-    public void gameStop() {
-        finish();
-    }
+    public void gameStop() { finish(); }
 
     public void gameStart() {
         ObjectAnimator clay_translateX = ObjectAnimator.ofFloat(iv_clay, "translationX", -100f, (float) screen_width);
         ObjectAnimator clay_translateY = ObjectAnimator.ofFloat(iv_clay, "translationY", 0f, 0f);
         ObjectAnimator clay_rotation = ObjectAnimator.ofFloat(iv_clay, "rotation", 0f, 360f * 5f);
+
         clay_translateX.setRepeatCount(NO_OF_CLAYS - 1);
         clay_translateY.setRepeatCount(NO_OF_CLAYS - 1);
         clay_rotation.setRepeatCount(NO_OF_CLAYS - 1);
+
         clay_translateX.setDuration(3000);
         clay_translateY.setDuration(3000);
         clay_rotation.setDuration(3000);
         clay_translateX.addListener(new Animator.AnimatorListener() {
             public void onAnimationStart(Animator animator) {
+
                 iv_clay.setVisibility(View.VISIBLE);
             }
 
@@ -139,6 +125,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         clay_translateX.start();
         clay_translateY.start();
         clay_rotation.start();
+    }
+    public void shootingStart(){
+        iv_bullet.setVisibility((View.VISIBLE));
+        ObjectAnimator bullet_scaleDownX = ObjectAnimator.ofFloat(iv_bullet,"scaleX",1.0f,0f);
+        ObjectAnimator bullet_scaleDownY = ObjectAnimator.ofFloat(iv_bullet,"scaleY",1.0f,0f);
+
+        double bullet_x = gun_center_x - bullet_width/2;
+        iv_bullet.setX((float) bullet_x);
+        ObjectAnimator bullet_translateX = ObjectAnimator.ofFloat(iv_bullet,"translationX", (float)bullet_x,(float)bullet_x);
+        ObjectAnimator bullet_translateY = ObjectAnimator.ofFloat(iv_bullet,"translationY", (float)gun_y,0f);
+        bullet_translateY.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                bullet_center_x = iv_bullet.getX() + bullet_width * 0.5f;
+                bullet_center_y = iv_bullet.getY() + bullet_height * 0.5f;
+
+                clay_center_x = iv_clay.getX() + clay_width * 0.5f;
+                clay_center_y = iv_clay.getY() + clay_height * 0.5f;
+
+                double dist = Math.sqrt(Math.pow(bullet_center_x - clay_center_x, 2) + Math.pow(bullet_center_y - bullet_center_y, 2));
+                if (dist <= 100) {
+                    iv_clay.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+        AnimatorSet bullet = new AnimatorSet();
+        bullet.playTogether(bullet_translateX, bullet_translateY, bullet_scaleDownX,bullet_scaleDownY);
+        bullet.start();
     }
 }
 
